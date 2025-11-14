fa5c4a6b47e042bdb3fe3a27fbe8b84c04ac54b6
Alejandro Alborno <233596652+alej-alborno@users.noreply.github.com>
2025-11-14 00:10:15 +0000

    add login session logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 const host = window.location.hostname;
-let baseUrl;
+let baseUrl, hostingPrefix;
 if (host === "localhost" || host.startsWith("127.") || host === "[::1]") {
     baseUrl = `http://${host}:3000`;
+    hostingPrefix = ""
 } else {
     baseUrl = "https://test-sm-website.fly.dev";
+    hostingPrefix = "/test-website"
 }
 
 async function checkBackend() {
@@ -22,23 +24,28 @@ async function checkBackend() {
 checkBackend()
 setInterval(checkBackend, 5000)
 
-async function register() {
-    const email = document.getElementById("email").value;
-    const res = await fetch(`${baseUrl}/register`, {
-        method: "POST",
+async function welc() {
+    const el = document.getElementById("welc");
+    const res = await fetch(`${baseUrl}/auth/validate`, {
+        method: "GET",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email })
+        credentials: "include"
     });
-    alert(await res.text());
+    if (res.ok) {
+        const data = await res.json();
+        el.innerText = el.innerText + " " + data.email;
+        alert(data.message);
+    }
 }
 
 async function signup() {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    const res = await fetch(`${baseUrl}/signup`, {
+    const res = await fetch(`${baseUrl}/auth/signup`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password })
+        body: JSON.stringify({ email, password }),
+        credentials: "include"
     });
     alert(await res.text());
 }
@@ -46,10 +53,38 @@ async function signup() {
 async function signin() {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    const res = await fetch(`${baseUrl}/signin`, {
+    const res = await fetch(`${baseUrl}/auth/signin`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password })
+        body: JSON.stringify({ email, password }),
+        credentials: "include"
+    });
+    if (res.ok) {
+        const params = new URLSearchParams(window.location.search);
+        const nextPage = params.get('next') || `${hostingPrefix}/`;
+        window.location.href = nextPage;
+    } else {
+        alert(await res.text());
+    }
+}
+
+async function cookiesignin() {
+    const res = await fetch(`${baseUrl}/auth/validate`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include"
     });
     alert(await res.text());
+}
+
+async function redirLoggedOut() {
+    const res = await fetch(`${baseUrl}/auth/validate`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include"
+    });
+    if (!res.ok) {
+        const returnUrl = encodeURIComponent(window.location.pathname);
+        window.location.href = `${hostingPrefix}/login?next=${returnUrl}`;
+    }
 }
